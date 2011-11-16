08e915b597e7d9540884b678e0f020b6f6e5f9cb
devilsansclue <ian.nusbaum@gmail.com>
2011-11-16 18:59:47 +0000

    start form

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,3 @@
-var querystring = require("querystring");
-
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
@@ -24,8 +22,7 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: "+
-  querystring.parse(postData).text);
+  response.write("You've sent: " + postData);
   response.end();
 }
 
