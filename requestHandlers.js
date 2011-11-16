b0135859bbe6f21670ffe4a0c5dda1bcd69b623f
devilsansclue <ian.nusbaum@gmail.com>
2011-11-16 18:53:08 +0000

    start form

--- requestHandlers.js
+++ requestHandlers.js
@@ -22,12 +22,13 @@ function start(response) {
 }
 
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + postData);
   response.end();
 }
+
 
 exports.start = start;
 exports.upload = upload;
--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-var http = require("http");
-var url = require("url");
+var querystring = require("querystring");
 
-function start(route, handle) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+function start(response, postData) {
+  console.log("Request handler 'start' was called.");
 
-    route(handle, pathname, response);
-  }
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
 
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+function upload(response, postData) {
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("You've sent the text: "+
+  querystring.parse(postData).text);
+  response.end();
 }
 
 exports.start = start;
+exports.upload = upload;
