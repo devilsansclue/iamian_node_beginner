a96136f60387fdcd97523a502505f58c3c8ce7cf
devilsansclue <ian.nusbaum@gmail.com>
2011-11-16 19:48:14 +0000

    start form

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,13 @@
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs = require("fs");
 
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
     '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
     '<form action="/upload" method="post">'+
@@ -29,5 +30,21 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
